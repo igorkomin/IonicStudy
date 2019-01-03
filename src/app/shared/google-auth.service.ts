bdf8f0487fe41841346ba1e19ee3b18ef77bfadd
Igor Komin <ik.kture@gmail.com>
2019-01-03 15:13:47 +0200

    remove unnecessary code

--- src/app/shared/google-auth.service.ts
+++ src/app/shared/google-auth.service.ts
@@ -3,7 +3,6 @@ import firebase from 'firebase';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { GooglePlus } from '@ionic-native/google-plus';
 import { Platform } from "ionic-angular";
-import { Toast } from '@ionic-native/toast';
 
 @Injectable()
 export class GoogleAuthService {
@@ -11,27 +10,17 @@ export class GoogleAuthService {
     private angularFireAuth: AngularFireAuth,
     private googlePlus: GooglePlus,
     private platform: Platform,
-    private toast: Toast
   ) { }
 
   private async nativeGooglelogin(): Promise<firebase.User> {
-    try {
-      const googleUser = await this.googlePlus.login({
-        'webClientId': '453175894946-8ne00gn111kr2t07ke712jj708c24k5c.apps.googleusercontent.com',
-        'offline': true,
-        'scopes': 'profile'
-      });
-      const credential = await this.angularFireAuth.auth.signInWithCredential(
-        firebase.auth.GoogleAuthProvider.credential(googleUser.idToken));
-      return credential;
-    }
-    catch (error) {
-      this.toast.show(error, '5000', 'bottom').subscribe(
-        toast => {
-          console.log(toast);
-        }
-      );
-    }
+    const googleUser = await this.googlePlus.login({
+      'webClientId': '453175894946-8ne00gn111kr2t07ke712jj708c24k5c.apps.googleusercontent.com',
+      'offline': true,
+      'scopes': 'profile'
+    });
+    const credential = await this.angularFireAuth.auth.signInWithCredential(
+      firebase.auth.GoogleAuthProvider.credential(googleUser.idToken));
+    return credential;
   }
 
   private async webGoogleLogin(): Promise<firebase.auth.UserCredential> {
@@ -40,38 +29,19 @@ export class GoogleAuthService {
   }
 
   public googleLogin(): Promise<any> {
-    try {
-      if (this.platform.is('cordova')) {
-        return this.nativeGooglelogin();
-      }
-      else {
-        return this.webGoogleLogin();
-      }
+    if (this.platform.is('cordova')) {
+      return this.nativeGooglelogin();
     }
-    catch (error) {
-      console.error(error);
+    else {
+      return this.webGoogleLogin();
     }
   }
 
   public getUserInfo(): firebase.User {
-    try {
-      return this.angularFireAuth.auth.currentUser;
-    }
-    catch (error) {
-      this.toast.show(error, '5000', 'bottom').subscribe(
-        toast => {
-          console.log(toast);
-        }
-      );
-    }
+    return this.angularFireAuth.auth.currentUser;
   }
 
   public signOut(): Promise<void> {
-    try {
-      return this.angularFireAuth.auth.signOut();
-    }
-    catch (error) {
-      console.error(error);
-    }
+    return this.angularFireAuth.auth.signOut();
   }
 }
