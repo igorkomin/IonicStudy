896c918a251b230b3bec78173c2364fb82af11d8
Igor Komin <ik.kture@gmail.com>
2019-01-03 14:40:57 +0200

    Add alert on login error

--- src/app/login/login/login.ts
+++ src/app/login/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { GoogleAuthService } from '../../shared/google-auth.service';
 import { HomePage } from '../../home/home/home';
 
@@ -9,13 +9,29 @@ import { HomePage } from '../../home/home/home';
 })
 export class LoginPage {
   constructor(public navCtrl: NavController, public navParams: NavParams,
+    private alertCtrl: AlertController,
     private authService: GoogleAuthService) {
   }
+
+  showAlert(title: string, message: string) {
+    const alert = this.alertCtrl.create({
+      title: title,
+      subTitle: message,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
   googleLogin() {
     this.authService.googleLogin().then(
       data => {
         if (data) {
           this.navCtrl.setRoot(HomePage);
+        }
+      },
+      error => {
+        if (error.code != 'auth/popup-closed-by-user') {
+          this.showAlert('Error!', error);
         }
       }
     );
